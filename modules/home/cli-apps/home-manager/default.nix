56fb870b11af9cdf94fdfc87042180f333e4c7b7
mainrs <5113257+mainrs@users.noreply.github.com>
2024-07-23 12:00:59 +0200

    feat(modules/home): add home-manager module

---
+++ modules/home/cli-apps/home-manager/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, namespace, ...}:
+
+let 
+  inherit (lib) mkEnableOption mkIf;
+  inherit (lib.${namespace}) enabled;
+
+  cfg = config.${namespace}.cli-apps.home-manager;
+in {
+  options.${namespace}.cli-apps.home-manager = {
+    enable = mkEnableOption "home-manager";
+  };
+
+  config = mkIf cfg.enable {
+    programs.home-manager = enabled;
+  };
+}
