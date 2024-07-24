9a731b64023730727367c46ebe819aa9e5820d8c
mainrs <5113257+mainrs@users.noreply.github.com>
2024-07-24 08:32:53 +0200

    feat(lib): add dummy package and option shorthand

--- lib/default.nix
+++ lib/default.nix
@@ -8,4 +8,13 @@
   enabled = {
     enable = true;
   };
+
+  mkOpt =
+    type: default: description:
+    lib.mkOption { inherit type default description; };
+  
+  dummyPackage = pkgs: pkgs.runCommand "dummy" { } ''
+    mkdir -p $out/bin
+    touch $out/bin/dummy
+  '';
 }
